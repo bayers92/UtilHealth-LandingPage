2177858631008126a706e5c3e9129dbc0c958475
bayers92 <brianc.ayers@gmail.com>
2015-09-11 13:09:26 -0400

    fixed tools images for mobile

--- subscribe.php
+++ subscribe.php
@@ -4,7 +4,7 @@
   $emailsFile = 'email-list.txt';
 
   //ENTER YOUR EMAIL ADDESS TO GET NOTIFIED EVERY TIME A SUBSCRIPTION IS DONE BY THE VISITORS.
-  $myEmail = '';
+  $myEmail = 'brianc.ayers@gmail.com';
 
 
   ob_start();
